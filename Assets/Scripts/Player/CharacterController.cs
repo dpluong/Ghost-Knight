e1348f8ee844a8032ee769bd51183b0cb415ad3a
dpluong <duyphuocnk92@gmail.com>
2023-12-25 17:59:06 -0800

    New input system testing

--- Assets/Scripts/Player/CharacterController.cs
+++ Assets/Scripts/Player/CharacterController.cs
@@ -1,16 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(PlayerInput))]
 public class CharacterController : MonoBehaviour
 {
+    public InputActionAsset actions;
+
+
+    private Rigidbody2D myRigidbody;
+    private Animator myAnimator;
+    private InputAction moveAction;
     
-    void Start()
+
+    void Awake()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+
+        moveAction = actions.FindActionMap("Player").FindAction("Move");
+
+        //actions.FindActionMap("Player").FindAction("Jump").performed += OnJump;
     }
 
-    
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+            Debug.Log("Jump!");
+    }
+
+    private void Move()
+    {
+        Vector2 moveVector = moveAction.ReadValue<Vector2>();
+        Debug.Log(moveVector);
+    }
+
+    void OnEnable()
+    {
+        actions.FindActionMap("Player").Enable();
+    }
+    void OnDisable()
+    {
+        actions.FindActionMap("Player").Disable();
+    }
+
     void Update()
     {
         
